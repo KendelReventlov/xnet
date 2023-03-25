c29eb5065d34b3e5c56035de7c16763934b89756
Eduardo Gracidas <eduardo@MacBook-Air-de-Eduardo-3.local>
2023-03-25 16:50:05 -0600

    j

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send("hola");
-});
+app.get("/", (req, res) => res.type('html').send("<html><body><p>Lorem ipsum</p></body></html>"));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
