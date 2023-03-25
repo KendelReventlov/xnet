6218e9efcb8258e016c12a67021f8a862cc8fb37
Eduardo Gracidas <eduardo@MacBook-Air-de-Eduardo-3.local>
2023-03-25 16:43:32 -0600

    a

--- main.js
+++ main.js
@@ -2,12 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001;
 
-app.use(express.static("./front"));
-
 app.get('/', (req, res) => {
   res.send("hola");
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
